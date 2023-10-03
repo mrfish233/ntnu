29924cfc11dcf83c0d23d783a548bf421f7b8a73
mrfish233 <baboom003@gmail.com>
2023-10-03 22:57:39 +0800

    [M] cp1 hw0105.c modified

--- class/cp1/hw01/README.md
+++ class/cp1/hw01/README.md
@@ -20,13 +20,4 @@ make
 
 # Something to tell TA
 
-### About hw0105
-
-The test part is not complete as I don't know how to complete it
-* Email's test is bad as some combination of domain will pass (eg. ta@cise.cool, 41247039s@nntu.deu.wt)
-* Email's test will skip subject's input if wrong local/domain
-* Subject's test will skip score's input if no category
-
-I will try to fix the bug above, but if I can't fix it I hope the correct part of the code get some points.
-Thank you TA.
-
+Nothing for now.
--- class/cp1/hw01/hw0105.c
+++ class/cp1/hw01/hw0105.c
@@ -1,186 +1,168 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main()
-{
-	// Read variables
-	int32_t read_email_local = 0, read_email_domain  = 0;
-	int32_t read_subject_title = 0;
-	uint64_t read_score = 0;
+int main() {
+	// Readed variables
+	int32_t emailLocalReaded = 0, emailDomainReaded = 0;
+	int32_t subjectCategoryReaded = 0, subjectTitleReaded = 0;
+	int32_t hwNum = 0, pNum = 0;
+	uint64_t contentScore = 0;
 
-	// Check variables
-	int32_t domain_length = 0;
+	// Validate variables
+	uint64_t subjectTitleScore = 0;
+	int32_t isEmailPassed = 0, isSubjectPassed = 0, isContentScorePassed = 0;
 
-	char hw_char = '\0', p_char = '\0';
-	int32_t hw_number = 0, p_number = 0;
-	int32_t assign_ta = 0;
-
-	uint64_t title_score = 0;
-
-	// Check result variables
-	int32_t is_email_passed = 0, is_subject_passed = 0, is_score_passed = 0;
+	// Assign variables
+	int32_t assignTA = 0;
 
 	/*
-	 * Scan inputs
+	 * Inputs 
 	 */
 
-	// Scan email
+	/* Scan email input*/
 	printf("Please enter the sender address      > ");
-	scanf("%*[a-zA-z0-9]@%n%*[ntnugappscsie].%*[ntnuedutw.cool]%n", &read_email_local, &read_email_domain);
 
-	//getchar();
+	// Validate email local
+	scanf("%*[a-zA-Z0-9]@%n", &emailLocalReaded);
 
-	// Scan subject
+	// Validate email domain
+	if (emailLocalReaded != 0) {
+		scanf("csie.cool%n", &emailDomainReaded);
+		scanf("ntnu.edu.tw%n", &emailDomainReaded);
+		scanf("gapps.ntnu.edu.tw%n", &emailDomainReaded);
+	}
+
+	// Clear input buffer
+	while (getchar() != '\n');
+
+	/* Scan subject input */
 	printf("Please enter the email subject       > ");
-	scanf(" %*3[[hwge]%c%*3[][erap]%c%*[]]%*[^\n]%n", &hw_char, &p_char, &read_subject_title);
 
-	//getchar();
+	// Validate subject category
+	scanf("[%n", &subjectCategoryReaded);
 
-	// Scan score
+	if (subjectCategoryReaded == 1) { /* If read "[" */
+		scanf("general]%n", &subjectCategoryReaded);
+		scanf("hw%d][p%d]%n", &hwNum, &pNum, &subjectCategoryReaded);
+	}
+
+	if (subjectCategoryReaded != 8) {	/* If wrong category  */
+		subjectCategoryReaded = 0;		/* Reset readed value to 0 */
+	}
+
+	// Validate subject title
+	scanf("%*[^\n]%n", &subjectTitleReaded);
+
+	// Clear input buffer
+	while (getchar() != '\n');
+
+	/* Scan content score */
 	printf("Please enter the email content score > ");
-	scanf(" %lu", &read_score); 
+	scanf("%lu", &contentScore);
+
+	// Clear input buffer
+	while (getchar() != '\n');
+
+	/* DEBUG */
+	// printf("local=%d, domain=%d\n", emailLocalReaded, emailDomainReaded);
+	// printf("category=%d, title=%d\n", subjectCategoryReaded, subjectTitleReaded);
+	// printf("hw=%d, p=%d\n", hwNum, pNum);
+	// printf("score=%lu\n", contentScore);
+
+	printf("================================================================================\n\n");
 
 	/*
-	 * Check inputs
+	 * Tests
 	 */
 	
-	// Domain length
-	domain_length = read_email_domain - read_email_local;
-	//printf("%d %d %d\n", read_email_local, read_email_domain, domain_length);
-
-	// Convert char to int
-	hw_number = hw_char - '0';
-	p_number  = p_char  - '0';
-	// TA assign
-	if (hw_char == 'n' && p_char == 'l')
-	{
-		assign_ta = 5;
-	}
-	else
-	{
-		assign_ta = (hw_number * p_number) % 5;
-	}
-	//printf("%c=%d %c=%d %d assign=%d\n", hw_char, hw_number, p_char, p_number, read_subject_title, assign_ta);
-
-	// Calculate title score to compare with read score
-	title_score = (read_subject_title - 11) * 10000000000;
-	//printf("%lu %lu\n", read_score, title_score);
-
-	/*
-	 * Validity Check
-	 */
-
-	// Check email validity
-	printf("\n================================================================================\n\n");
+	/* Email test */
 	printf("Sender Address Test : ");
-	
-	// Check local part
-	if (read_email_local == 0)
-	{
-		printf("Failed, local-part is invalid\n"); 
-	}
-	// Check domain part
-	else if (read_email_domain == 0 ||
-			 (domain_length != 9 && domain_length != 11 && domain_length != 16 && domain_length != 17))
-	{
-		printf("Failed, domain is not authorised\n");
-	}
-	// Email is correct
-	else
-	{
-		is_email_passed = 1;
-		printf("Passed\n");
-	}
 
-	// Check subject validity
+	if (!emailLocalReaded) {
+		printf("Failed, local-part is invalid"); 
+	}
+	else if (!emailDomainReaded) {
+		printf("Failed, domain is not authorised");
+	}
+	else {
+		isEmailPassed = 1;
+		printf("Passed");
+	}
+	printf("\n");
+
+	/* Subject test */
 	printf("Email Subject Test  : ");
-	
-	// Skip test if email test failed
-	if (!is_email_passed)
-	{
-		printf("-\n");
-	}
-	// Check category part
-	else if (hw_number < 0|| p_number < 0)
-	{
-		printf("Failed, no category\n");
-	}
-	// Check title part
-	else if (read_subject_title == 0)
-	{
-		printf("Failed, title is empty\n");
-	}
-	// Subject is correct
-	else
-	{
-		is_subject_passed = 1;
-		printf("Passed\n");
-	}
 
-	// Check score validity
+	if (!isEmailPassed) { /* Skip test if email fail*/
+		printf("-");
+	}
+	else if (!subjectCategoryReaded) {
+		printf("Failed, no category");
+	}
+	else if (!subjectTitleReaded) {
+		printf("Failed, title is empty");
+	}
+	else {
+		isSubjectPassed = 1;
+		printf("Passed");
+	}
+	printf("\n");
+
+	/* Content score test */
+	// Title score
+	subjectTitleScore = (subjectTitleReaded - 1) * 10000000000;
+
 	printf("Email Content Test  : ");
 
-	// Skip test if subject test failed
-	if (!is_subject_passed)
-	{
-		printf("-\n");
+	if (!isSubjectPassed) {
+		printf("-");
 	}
-	// Check score range
-	else if (read_score < 0 || read_score > 10000000000000000000UL)
-	{
-		printf("Failed, out of range\n");
+	else if (contentScore > 10000000000000000000UL) {
+		printf("Failed, out of range");
 	}
-	// Compare title score and content score
-	else if (read_score < title_score)
-	{
-		printf("Failed, too low\n");
+	else if (contentScore < subjectTitleScore) {
+		printf("Failed, too low");
 	}
-	// Score is correct
-	else
-	{
-		is_score_passed = 1;
-		printf("Passed\n");
+	else {
+		isContentScorePassed = 1;
+		printf("Passed");
 	}
+	printf("\n");
+
+	printf("--------------------------------------------------------------------------------\n\n");
 
 	/*
-	 * Assign or Reject
+	 * TA Assignments
 	 */
-	printf("\n--------------------------------------------------------------------------------\n\n");
+	if (hwNum == 0 && pNum == 0) {
+		assignTA = 5; /* General assings to QB */
+	}
+	else {
+		assignTA = (hwNum * pNum) % 5; /* Homework assings to 1 of 5 TAs */
+	}
 
-	// Reject if any failed test
-	if (!(is_email_passed && is_subject_passed && is_score_passed))
-	{
-		printf("Rejected\n");
+	if (!(isEmailPassed && isSubjectPassed && isContentScorePassed)) {
+		printf("Rejected");
 	}
-	// General assign to QB
-	else if (assign_ta == 0)
-	{
-		printf("Assigned to Kaname Madoka\n");
+	else if (assignTA == 0) {
+		printf("Assigned to Kaname Madoka");
 	}
-	else if (assign_ta == 1)
-	{
-		printf("Assigned to Akemi Homura\n");
+	else if (assignTA == 1) {
+		printf("Assigned to Akemi Homura");
 	}
-	else if (assign_ta == 2)
-	{
-		printf("Assigned to Miki Sayaka\n");
+	else if (assignTA == 2) {
+		printf("Assigned to Miki Sayaka");
 	}
-	else if (assign_ta == 3)
-	{
-		printf("Assigned to Tomoe Mami\n");
+	else if (assignTA == 3) {
+		printf("Assigned to Tomoe Mami");
 	}
-	else if (assign_ta == 4)
-	{
-		printf("Assigned to Sakura Kyoko\n");
+	else if (assignTA == 4) {
+		printf("Assigned to Sakura Kyoko");
 	}
-	else if (assign_ta == 5)
-	{
-		printf("Assigned to QB\n");
+	else if (assignTA == 5) {
+		printf("Assigned to QB");
 	}
-	else
-	{
-		printf("Rejected\n");
-	}
+	printf("\n");
 
 	return 0;
 }
