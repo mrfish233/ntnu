e7abb69a88ee65a63fa15ade5dd72abc92c94e41
mrfish233 <baboom003@gmail.com>
2023-10-27 19:49:21 +0800

    [A] hw03 README and test.sh added

---
+++ class/cp1/hw03/test.sh
@@ -0,0 +1,16 @@
+make
+
+for i in $(seq 1 5); do
+	j=0
+	for file in test/0$i/in/*.in; do
+		if [ $j -lt 10 ]; then
+			./hw030$i < $file  > test/0$i/out/0$j.out
+		else
+			./hw030$i < $file  > test/0$i/out/$j.out
+		fi
+
+		j=$((j+1))
+	done
+
+	echo "test0$i done"
+done
